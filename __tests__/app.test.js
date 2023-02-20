2aef83aa63a5db3701ee2e5e1659d85a22bcded7
JoshFost <josh.foster@hotmail.co.uk>
2023-02-20 15:24:33 +0000

    made changes due to feedback for 3./api/categories

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,26 +16,20 @@ beforeEach(() => {
 });
 
 describe("app", () => {
-  describe("/api", () => {
-    it("200: GET - responds with server OK message", () => {
-      return request(app) //ARRANGE
-        .get("/api") // ACT
-        .expect(200) //ASSERT ---- Supertest
-        .then((response) => {
-          expect(response.body.msg).toBe("all ok");
-        });
-    });
-  });
   describe("/api/categories", () => {
     it("200: GET - responds with an array of category objects", () => {
       return request(app)
         .get("/api/categories")
         .expect(200)
         .then((response) => {
-          //   console.log(response.body.categories);
+          expect(Array.isArray(response.body.categories)).toBe(true);
           expect(response.body.categories.length).toBe(7);
-          expect(response.body.categories[0]).toHaveProperty("slug");
-          expect(response.body.categories[0]).toHaveProperty("description");
+          response.body.categories.forEach((category) => {
+            expect(category).toHaveProperty("slug");
+            expect(category).toHaveProperty("description");
+            expect(typeof category.slug).toBe("string");
+            expect(typeof category.description).toBe("string");
+          });
         })
         .catch((err) => {
           console.log(err);

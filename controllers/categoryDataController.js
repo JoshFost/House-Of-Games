2d50ae747851517121ff64af1815da569b0f7863
JoshFost <josh.foster@hotmail.co.uk>
2023-02-24 11:22:39 +0000

    checking out to branch 8

--- controllers/categoryDataController.js
+++ controllers/categoryDataController.js
@@ -38,15 +38,6 @@ exports.getReviewById = (req, res, next) => {
       next(err);
     });
 };
-/*
-currently in main.
-resubmitted a pr for branch 6, not yet merged
-started branch 7 but stashed all code
-to do...
-get branch 6 feedback and merge to main
-continue with 7
-submit pr and merge with main and solve conflicts
-*/
 
 exports.getCommentsByReviewId = (req, res, next) => {
   const { review_id } = req.params; // destructure

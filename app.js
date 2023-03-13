ec489d33c2d0a4e79089a63b2d79882a0e87ce64
JoshFost <josh.foster@hotmail.co.uk>
2023-03-13 16:15:52 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 //get from the controller
 const {
   getCategoryData,
@@ -18,6 +19,8 @@ const {
   handlePsqlErrors,
   handleReviewNotFoundError,
 } = require("./controllers/errorHandler");
+
+app.use(cors());
 
 app.use(express.json());
 
